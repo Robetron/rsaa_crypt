d849bf05d87f9665a81bf83df4b419575eade269
Vishal Robertson <vishalsnedan@gmail.com>
2020-02-22 18:19:16 +0530

    Minor cleanup

--- test/rsaa_crypt_test.dart
+++ test/rsaa_crypt_test.dart
@@ -2,17 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rsaa_crypt/rsaa_crypt.dart';
 
 void main() {
-  RSAACrypt rsaa = RSAACrypt();
-
   group('Text Encryption and Decryption', () {
     test('values should match', () {
       String message = 'Hello! Cześć! 🐻🎌 你好! ご挨拶！Привет! ℌ𝔢𝔩𝔩𝔬!';
       String key = '🐻🎌';
 
+      print('Key: $key');
       print('Message Text: $message');
-      String cipher = rsaa.encryptText(key, message);
+      String cipher = RSAACrypt().encryptText(key, message);
       print('Cipher Text: $cipher');
-      String output = rsaa.decryptText(key, cipher);
+      String output = RSAACrypt().decryptText(key, cipher);
       print('Decrypted Text: $message');
       expect(message, output);
     });
